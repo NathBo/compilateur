509d92fcf85ff59dd32e32fefead14461b275768
Vincent <j.vinc-sites@yahoo.com>
2024-01-01 19:25:07 +0100

    nouveau tests et code mieux organisé

--- src/purescript_production_code.ml
+++ src/purescript_production_code.ml
@@ -8,26 +8,24 @@ let code_initial =
         call "printf" ++
         ret
 
+let rec traduit_tddecl = function
+        | TDtdecl _ -> nop
+        | TDdefn tdefn -> traduit_tdefn tdefn
+        | _ -> failwith "ne sait pas encore faire"
+and traduit_tdefn tdefn =
+        label tdefn.tident ++
+        traduit_texpr tdefn.texpr ++
+        movq (imm 0) (reg rax) ++
+        ret
+and traduit_texpr = function
+        | TElident (fct, args, typ) ->
+                call fct
+        | _ -> failwith "ne sait pas encore faire"
+
+
 
 let genere_code arbre_typage =
     let data = (label "printf_log") ++ (string "abdef\n") in
-    let text = List.fold_left (fun acc tvdec -> acc ++ match tvdec with
-        | TDtdecl _ -> nop
-        | TDdefn tdefn ->
-                label tdefn.tident ++
-                begin match tdefn.texpr with
-                | TElident (fct, args, typ) ->
-                        call fct
-                | _ -> 
-                        label tdefn.tident ++
-                        movq (imm 42) (reg rdi) ++
-                        call "log" ++
-                        movq (imm 0) (reg rax) ++
-                        ret
-                end ++
-                movq (imm 0) (reg rax) ++
-                ret
-        | _ -> failwith "ne sait pas encore faire"
-    ) code_initial arbre_typage in
+    let text = List.fold_left (fun acc tvdec -> acc ++ traduit_tddecl tvdec ) code_initial arbre_typage in
                    
     {text = text ; data = data}
